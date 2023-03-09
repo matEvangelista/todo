2a48bfd01c2b38d595090d5ba0c7ded64e0c3088
Mateus Alcantara <mateus.e.alcantara@outlook.com>
2023-03-09 09:33:39 -0300

    Adicionando botão de remover

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,47 +7,69 @@ const Task = styled.div`
     span {
         text-decoration: ${props => props.done ? "line-through" : "none"};
     }
+    display: ${props => props.showCard ? "flex" : "none"};
     font-size: 1.2rem;
     background-color: yellow;
     padding: 2rem;
     box-shadow: 3px 3px 3px black;
     text-align: center;
     align-items: center;
-    display: flex;
     flex-direction: column;
     button {
         padding: .3rem;
         font-size: 1.2rem;
         color: white;
-        background-color: rgb(52, 74, 141);
         transition: .3s;
         border-radius: 10px;
         border: none;
-        text-decoration: none !important;
         margin-top: 2rem;
         &:hover {
-            background-color: rgb(15, 1, 93);
             cursor: pointer;
+        }
+    }
+    .concluir {
+        background-color: rgb(52, 74, 141);
+        &:hover {
+            background-color: rgb(15, 1, 93);
         }
         &:disabled {
             background-color: rgba(11, 108, 0, 0.688);
             cursor:no-drop;
         }
     }
+    .remover {
+        background-color: red;
+        &:hover {
+            background-color: darkred;
+        }
+    }
+    div {
+        display: flex;
+        max-width: 300px;
+        width: 100%;
+        justify-content: space-around;
+    }
 `
 
 export default function Item({ task }) {
 
     const [disable, setDisable] = useState(false)
+    const [show, setShow] = useState(true)
 
     return (
-        <Task done={disable}>
+        <Task done={disable} showCard={show}>
             <span>
                 {task}
             </span>
-            <button disabled={disable} onClick={() => {
-                setDisable(!disable)
-            }}>{disable ? "Feito :)" : "Terminei"}</button>
+            <div>
+                <button className="concluir" disabled={disable} onClick={() => {
+                    setDisable(!disable)
+                }}>{disable ? "Feito :)" : "Terminei"}</button>
+
+                <button className="remover" showButton={show} onClick={() => {
+                    setShow(false)
+                }}>Excluir</button>
+            </div>
         </Task>
     )
 
